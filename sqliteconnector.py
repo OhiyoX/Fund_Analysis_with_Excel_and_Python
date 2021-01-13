f84479487b935f5f058219c262a7c9f8092a93af
早新 <ohiyox@gmail.com>
2021-01-13 17:17:08 +0800

    support sqlite

--- sqliteconnector.py
+++ sqliteconnector.py
@@ -21,29 +21,9 @@ class FundSqlite:
         if self.conn is not None:
             self.conn.close()
 
-    def update_fund_info(self,curcor,id,code,word,name,type):
-        exec_script = f"""
-        INSERT OR REPLACE INTO FUND_INFO (ID,CODE,WORD,NAME,TYPE)
-        VALUES ({id}, '{code}', '{word}', '{name}', '{type}');
-        """
-        curcor.execute(exec_script)
-
-    def update_fund_record(self,curcor,code,date,networth):
-        date_f = date.replace('-','')
-        codetime = code + date_f
-
-        exec_script = f"""
-        INSERT OR REPLACE INTO FUND_RECORD
-        (CODE, DATE, NETWORTH,CODETIME)
-        VALUES
-        ('{code}','{date}',{networth},{codetime});
-        """
-
-        curcor.execute(exec_script)
-
-
 
 def make_fund_info():
+    # 从csv导入基金基本信息：代码、名称等
     csv_path = r"D:\Coding\LearnPython\Automatic\Fund-Analysis\resource\fundcode_info\fundcode_search.csv"
     sqlc = FundSqlite()
     sqlc.connect()
@@ -55,12 +35,18 @@ def make_fund_info():
         word = row[1]['WORD']
         name = row[1]['NAME']
         type = row[1]['TYPE']
-        sqlc.update_fund_info(curcor=c,id=id,code=code,word=word,name=name,type=type)
+        exec_script = f"""
+        INSERT OR REPLACE INTO FUND_INFO (ID,CODE,WORD,NAME,TYPE)
+        VALUES ({id}, '{code}', '{word}', '{name}', '{type}');
+        """
+        c.execute(exec_script)
     print('Done')
     sqlc.conn.commit()
     sqlc.close()
 
+
 def make_fund_worth_record():
+    #csv导入基金信息记录，净值、收益率等
     sqlc = FundSqlite()
     sqlc.connect()
     c = sqlc.conn.cursor()
@@ -75,14 +61,36 @@ def make_fund_worth_record():
         for i in range(0, len(date_col)):
             date = date_col[i]
             networth = round(tmp[i],4)
-            sqlc.update_fund_record(curcor=c,code=col,date=date,networth=networth)
+            code =col
+
+            date_f = date.replace('-', '')
+            codetime = code + date_f
+
+            exec_script = f"""
+            INSERT OR REPLACE INTO FUND_RECORD
+            (CODE, DATE, NETWORTH,CODETIME)
+            VALUES
+            ('{code}','{date}',{networth},{codetime});
+            """
+            c.execute(exec_script)
     sqlc.conn.commit()
     sqlc.close()
 
 def add_increase_rate():
+    # 导入收益率
     sqlc = FundSqlite()
     sqlc.connect()
     c= sqlc.conn.cursor()
+
+    try:
+        add_col_script = f"""
+        ALTER TABLE FUND_RECORD
+        ADD COLUMN INCREASE_RATE REAL;
+        """
+        c.execute(add_col_script)
+    except:
+        c.callback()
+    sqlc.conn.commit()
 
     csv_path = r"D:\Coding\LearnPython\Automatic\Fund-Analysis\resource\csv\fund-worth-increase-data.csv"
     data = pd.read_csv(csv_path)
@@ -106,8 +114,8 @@ def add_increase_rate():
             # res = c.execute(check_sql).fetchone()
             # if res != 0:
             sql_script = f"""
-            UPDATE FUND_RECORD
-            SET INCREASE_RATE = {round(increase_rate,4) if not pd.isna(increase_rate) else 0}
+            UPDATE FUND_RECORD 
+            SET INCREASE_RATE = {round(increase_rate,4) if not pd.isna(increase_rate) else 0} 
             WHERE CODE = '{code}' AND DATE = '{date}' AND INCREASE_RATE IS NOT NULL;
             """
             try:
@@ -126,5 +134,55 @@ def add_increase_rate():
     sqlc.conn.commit()
     sqlc.close()
 
+def add_acc_worth():
+    # 导入 累计净值
+    sqlc = FundSqlite()
+    sqlc.connect()
+    c= sqlc.conn.cursor()
+    try:
+        add_col_script = f"""
+        ALTER TABLE FUND_RECORD
+        ADD COLUMN ACCWORTH REAL;
+        """
+        c.execute(add_col_script)
+    except:
+        c.callback()
+    sqlc.conn.commit()
+
+    csv_path = r"D:\Coding\LearnPython\Automatic\Fund-Analysis\resource\csv\fund-acc-worth-data.csv"
+    data = pd.read_csv(csv_path)
+    cols = list(data.columns)
+    cols = cols[1:]
+    date_col = data['DATE']
+    workload = len(date_col)*len(cols)
+    progress = 0
+    for col in cols:
+        tmp = data[col]
+        for i in range(0,len(date_col)):
+            code = col
+            date = date_col[i]
+            acc_worth = round(tmp[i],4)
+
+            sql_script = f"""
+            UPDATE FUND_RECORD
+            SET ACCWORTH = {acc_worth if not pd.isna(acc_worth) else "NULL"} 
+            WHERE CODE = '{code}' AND DATE = '{date}' AND ACCWORTH IS NULL;
+            """
+            try:
+                c.execute(sql_script)
+                progress += 1
+                __show_progress(progress, workload)
+            except Exception as err_info:
+                print(err_info)
+                print(pd.isna(acc_worth))
+                # print(res)
+                print(code)
+                print(date)
+                print(acc_worth)
+                print(sql_script)
+                exit(0)
+    sqlc.conn.commit()
+    sqlc.close()
+
 if __name__ == '__main__':
-    add_increase_rate()
+    add_acc_worth()
